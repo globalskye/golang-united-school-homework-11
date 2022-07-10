08d8d31cf2e514dd42372f28c3e361c50825b632
global <asgagagsad@gmail.com>
2022-07-10 19:15:33 +0300

    finish?

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,28 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	ch := make(chan struct{}, pool)
+
+	var i int64
+
+	for i = 0; i < n; i++ {
+		wg.Add(1)
+		ch <- struct{}{}
+		go func(i int64) {
+			usr := getOne(i)
+			<-ch
+			mu.Lock()
+			res = append(res, usr)
+			mu.Unlock()
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	return
 }
